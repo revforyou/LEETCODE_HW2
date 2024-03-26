cc844faefb1af0acf322a8dadf947c0f72e71a87
revforyou <103939888+revforyou@users.noreply.github.com>
2024-03-26 11:21:44 -0400

    Update Problem2_W2.py

--- Problem2_W2.py
+++ Problem2_W2.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxlen, largestCount = 0, 0
+        arr = collections.Counter()
+        for i in range(len(s)):
+            arr[s[i]] += 1
+            largestCount = max(largestCount, arr[s[i]])
+            if maxlen - largestCount >= k:
+                arr[s[i - maxlen]] -= 1
+            else:
+                maxlen += 1
+        return maxlen
